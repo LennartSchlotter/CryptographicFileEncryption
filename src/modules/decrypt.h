b816d09c9ada2af825ac530bdd2194c8f10e1605
Lennart Schlotter <lennschlott@gmail.com>
2026-03-25 23:35:35 +0100

    feat: add contracts for cryptography functions

--- src/modules/decrypt.h
+++ src/modules/decrypt.h
@@ -4,11 +4,21 @@
 #define DECRYPT_H
 
 #include "handler.h"
+#include "helper/secure_allocator.h"
 
 namespace crypto {
 
 // Handles decrypting the file passed in the request.
 Result decrypt(const DecryptRequest& request);
+
+// Handles preparing the necessary requirements for symmetric decryption.
+std::vector<char, SecureAllocator> prepare_symmetric();
+
+// Handles preparing the necessary requirements for asymmetric decryption.
+std::vector<char, SecureAllocator> prepare_asymmetric();
+
+// Decrypts the passed file using the provided algorithm and key.
+void decrypt(std::ofstream file, CryptoAlgorithms algorithm, std::vector<char, SecureAllocator> key);
 }  // namespace crypto
 
 #endif
--- src/modules/encrypt.h
+++ src/modules/encrypt.h
@@ -3,11 +3,21 @@
 #define ENCRYPT_H
 
 #include "handler.h"
+#include "helper/secure_allocator.h"
 
 namespace crypto {
 
 // Handles encrypting the file passed in the request.
 Result encrypt(const EncryptRequest& request);
+
+// Handles preparing the necessary requirements for symmetric encryption.
+std::vector<char, SecureAllocator> prepare_symmetric(std::ofstream& result_file);
+
+// Handles preparing the necessary requirements for asymmetric encryption.
+std::vector<char, SecureAllocator> prepare_asymmetric(std::ofstream& result_file);
+
+// Encrypts the passed file using the provided algorithm and key.
+void encrypt(std::ofstream file, CryptoAlgorithms algorithm, std::vector<char, SecureAllocator> key);
 }  // namespace crypto
 
 #endif
