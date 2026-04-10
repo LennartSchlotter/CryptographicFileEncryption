9936f5b311cbac95223b2d02a9cfc604a3eb46c6
Lennart Schlotter <lennschlott@gmail.com>
2026-04-10 14:49:01 +0200

    fix: contracts

--- src/modules/encrypt.h
+++ src/modules/encrypt.h
@@ -3,7 +3,6 @@
 #define ENCRYPT_H
 
 #include "handler.h"
-#include "helper/secure_allocator.h"
 
 namespace crypto {
 
@@ -11,13 +10,13 @@ namespace crypto {
 Result encrypt(const EncryptRequest& request);
 
 // Handles preparing the necessary requirements for symmetric encryption.
-std::vector<char, SecureAllocator> prepare_symmetric(std::ofstream& result_file);
+std::vector<uint8_t> prepare_symmetric(std::ofstream& result_file);
 
 // Handles preparing the necessary requirements for asymmetric encryption.
-std::vector<char, SecureAllocator> prepare_asymmetric(std::ofstream& result_file);
+std::vector<uint8_t> prepare_asymmetric(std::ofstream& result_file);
 
 // Encrypts the passed file using the provided algorithm and key.
-void encrypt(std::ofstream file, CryptoAlgorithms algorithm, std::vector<char, SecureAllocator> key);
+void encrypt(std::ofstream& file, CryptoAlgorithms algorithm, std::vector<uint8_t> key);
 }  // namespace crypto
 
 #endif
