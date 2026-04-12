06cb958c7ca1c23bafed8dfe9d56a4e6f8b468c1
Lennart Schlotter <lennschlott@gmail.com>
2026-04-12 10:25:37 +0200

    chore: provide documentation for the allocator contract

--- src/helper/secure_allocator.h
+++ src/helper/secure_allocator.h
@@ -24,8 +24,14 @@ class SecureAllocator {
         using other = SecureAllocator;
     };
 
+    // Allocates memory for the passed amount of chars.
     char* allocate(size_t n);
+
+    // Allocates at least the passed amount of chars.
+    // Returns an allocation result containing a pointer to the allocated memory and the actual number of elements.
     std::allocation_result<char*> allocate_at_least(size_t n);
+
+    // Provides a way to deallocate memory using the built-in `secure_zero` helper.
     void deallocate(char* ptr, size_t n);
 };
 
