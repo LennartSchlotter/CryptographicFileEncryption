241f6b34a07436c72a6b974e10844ede60e8b3ab
Lennart Schlotter <lennschlott@gmail.com>
2026-05-01 19:56:28 +0200

    chore: adjust contract for decryption

--- src/modules/decrypt.h
+++ src/modules/decrypt.h
@@ -12,13 +12,13 @@ namespace crypto {
 Result decrypt(const DecryptRequest& request);
 
 // Handles preparing the necessary requirements for symmetric decryption.
-std::vector<char, SecureAllocator<char>> prepare_symmetric();
+std::vector<uint8_t, SecureAllocator<uint8_t>> prepare_symmetric(const std::vector<char, SecureAllocator<char>>& encrypted_file_content);
 
 // Handles preparing the necessary requirements for asymmetric decryption.
-std::vector<char, SecureAllocator<char>> prepare_asymmetric();
+std::vector<uint8_t, SecureAllocator<uint8_t>> prepare_asymmetric(const std::vector<char, SecureAllocator<char>>& encrypted_file_content, const CryptoAlgorithms& algorithm);
 
 // Decrypts the passed file using the provided algorithm and key.
-void decrypt(std::ofstream file, CryptoAlgorithms algorithm, std::vector<char, SecureAllocator<char>> key);
+void decrypt(const std::vector<char, SecureAllocator<char>>& encrypted_file_content, const std::string& output_path, const CryptoAlgorithms& algorithm, const std::vector<unsigned char, SecureAllocator<unsigned char>>& key);
 }  // namespace crypto
 
 #endif
